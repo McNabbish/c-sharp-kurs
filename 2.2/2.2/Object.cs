401bafce655a3b454766394d39289cf8282c708d
McNabbish <43358326+McNabbish@users.noreply.github.com>
2019-02-07 12:30:54 +0100

    Add files via upload

---
+++ 2.2/2.2/Object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    class Object
+    {
+        public static int nObj = 0;
+        Random RNG = new Random();
+        readonly private int ID;
+
+       public Object()
+        {
+            nObj++;
+            ID = RNG.Next(0, 1000000 + 1);
+
+            Console.WriteLine("Object with ID: '{0}' created", ID);
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+
+    }
+}
---
+++ 2.2/2.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ObjList = new List<Object>();
+            int c=0;
+
+            while (true)
+            {
+                Console.WriteLine("To spawn object, enter 1, To find number of existing objects enter 2, to print unique id of all objects enter 3:");
+                try
+                {
+                    c = Convert.ToInt32(Console.ReadLine());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("input error: {0}", e);
+                }
+
+                switch (c)
+                {
+                    case 1:
+                        ObjList.Add(new Object());
+                        break;
+                    case 2:
+                        Console.WriteLine("Number of objects created: {0}", Object.nObj);
+                        break;
+                    case 3:
+                        foreach (Object T in ObjList)
+                        {
+                            Console.WriteLine(T.GetID());
+                        }
+                        break;
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
