70da053ac7d28dba059cb3d67b603a4acbe93f63
McNabbish <43358326+McNabbish@users.noreply.github.com>
2019-02-07 01:17:16 +0100

    Add files via upload

---
+++ ConsoleApp1/Frame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fr
+{
+    public class Frame
+    {
+        public Frame(int a, int b)
+        {
+            int First = 0;
+            int Second = 0;
+        }
+
+        public bool IsStrike()
+        {
+            if (First == 10)
+                return true;
+            else
+                return false;
+        }
+
+        public bool IsSpare()
+        {
+            if (First + Second == 10)
+                return true;
+            else
+                return false;
+        }
+
+    }
+
+}
+
