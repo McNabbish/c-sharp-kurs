518672e3dd192bf3e01ed2d074969f22eb38e85e
McNabbish <43358326+McNabbish@users.noreply.github.com>
2019-02-21 21:41:19 +0100

    Add files via upload

---
+++ sudokutesting2.0/sudokutesting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using sudokutesting;
+
+namespace sudokutesting
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            SudokuLoader a = new SudokuLoader();
+            string[][] SudokuBoard = a.Load(@"C:\Users\Johan\Desktop\c-sharp-kurs-master\sudokutesting\input2.txt");
+
+            Board Sudoku = new Board(SudokuBoard);
+
+            Sudoku.solve();
+
+            Console.ReadLine();
+        }
+    }
+ }   
+
+
