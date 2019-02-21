3a50dad91b377f9910e67555839ca2fc40dee3b4
McNabbish <43358326+McNabbish@users.noreply.github.com>
2019-02-21 17:51:15 +0100

    Add files via upload

---
+++ sudokutesting/sudokutesting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using sudokutesting;
+
+namespace sudokutesting
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            SudokuLoader a = new SudokuLoader();
+            string[][] SudokuBoard = a.Load(@"C:\Users\johan\Documents\EasySudoku.txt");
+
+            Board Sudoku = new Board(SudokuBoard);
+
+            Sudoku.solve();
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+ }   
+
+
