86ffcfce8d0f9961aac9b6b9d1cb3b80a1f34338
McNabbish <43358326+McNabbish@users.noreply.github.com>
2019-02-21 21:37:36 +0100

    Add files via upload

---
+++ lisajour/lisajour/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace lisajour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Series1FDial = new System.Windows.Forms.NumericUpDown();
+            this.Series2FDial = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series1FDial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series2FDial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 35);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "Series2";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(502, 341);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "LisajouFigure";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Series1FDial
+            // 
+            this.Series1FDial.Location = new System.Drawing.Point(12, 429);
+            this.Series1FDial.Name = "Series1FDial";
+            this.Series1FDial.Size = new System.Drawing.Size(120, 20);
+            this.Series1FDial.TabIndex = 2;
+            this.Series1FDial.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Series1FDial.ValueChanged += new System.EventHandler(this.Series1FDial_ValueChanged);
+            // 
+            // Series2FDial
+            // 
+            this.Series2FDial.Location = new System.Drawing.Point(394, 429);
+            this.Series2FDial.Name = "Series2FDial";
+            this.Series2FDial.Size = new System.Drawing.Size(120, 20);
+            this.Series2FDial.TabIndex = 3;
+            this.Series2FDial.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Series2FDial.ValueChanged += new System.EventHandler(this.Series2FDial_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Series 1 frequency";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Series 2 frequency";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(145, 382);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Hide legend";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 484);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Series2FDial);
+            this.Controls.Add(this.Series1FDial);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series1FDial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Series2FDial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown Series1FDial;
+        private System.Windows.Forms.NumericUpDown Series2FDial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ lisajour/lisajour/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lisajour
+{
+    public partial class Form1 : Form
+    {
+        double[] x1 = new double[100]; //datavalues for series 1
+        double[] y1 = new double[100];
+        double f1 = 1;
+
+        double[] x2 = new double[100]; // data values for series 2
+        double[] y2 = new double[100];
+        double f2 = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // draws figure without changing any values
+        {
+            Draw();
+        }
+
+        private void Series1FDial_ValueChanged(object sender, EventArgs e) //changes frequency of series 1
+        {
+            f1 = (double)Series1FDial.Value;
+            Draw();
+        }
+
+        private void Series2FDial_ValueChanged(object sender, EventArgs e) // changes frequency of series 2
+        {
+            f2 = (double)Series2FDial.Value;
+            Draw();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) // hides/displays legend
+        {
+            if (checkBox1.Checked)
+            {
+                chart1.Series[0].IsVisibleInLegend = false;
+                chart1.Series[1].IsVisibleInLegend = false;
+            }
+            else
+            {
+                chart1.Series[0].IsVisibleInLegend = true;
+                chart1.Series[1].IsVisibleInLegend = true;
+            }
+        }
+
+
+        //helper functions
+        private void Draw() // updates charts with lisabon figures
+        {
+
+            chart1.Series["Series1"].Points.Clear();
+            chart1.Series["Series2"].Points.Clear();
+
+            for (int i = 0; i < 100; i++) // plotting series 1
+            {
+                x1[i] = -1 * Math.PI + 0.02 * i * Math.PI;
+                y1[i] = Math.Sin(x1[i] * f1);
+
+                chart1.Series["Series1"].Points.AddXY(y1[i], -1+0.02*i);
+            }
+
+            for (int i = 0; i < 100; i++) // plotting series 2
+            {
+                x2[i] = -1 * Math.PI + 0.02 * i * Math.PI;
+                y2[i] = Math.Sin(x2[i] * -f2);
+
+                chart1.Series["Series2"].Points.AddXY(y2[i], -1 + 0.02 * i);
+            }
+        }
+
+
+    }
+}
